fb473bd438607542c3d27af41382f7f2cc036740
codewithap <99323868+codewithap@users.noreply.github.com>
2023-02-19 18:15:01 +0530

    Add files via upload

---
+++ api/main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+from scrappers.manganelo import manganelo
+from scrappers.myanimelist import myanimelist
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+@app.route("/")
+def home():
+  
+  return ""
+
+@app.route("/api/v1/search")
+def search():
+  query = request.args.get("q")
+  data = myanimelist.search(query)
+  return data
+  
+  
+@app.route("/api/v1/anime")
+def anime():
+  chapters = request.args.get("id")
+  return ""
+
+
+
+if __name__ == "__main__":
+  app.run(host = "0.0.0.0", debug=True)
--- scrappers/myanimelist.py
+++ scrappers/myanimelist.py
@@ -10,20 +10,27 @@ class myanimelist:
         pass
 
     def search(query):
-        r = get(base_url+ query)
-        soup = bs(r.text,"html.parser")
-        list = soup.select(".list table tr")
-        data = '{"data": ['
-        # list.pop(0)
-        for item in list:
-            print(item,"\n\n")
-        #     link = item.find("a")["href"]
-        #     img = item.find("img")["src"]
-        #     title = item.findAll("a")[1].text
-        #     info = "{" + f'"img" :"{img}", "title": "{title}", "link": "{link}"'+ "},"
-        #     data = data + info
-        # jsonData = data[:-1] + ']}'
-        return list#json.loads(jsonData)
+      r = get(base_url+ query)
+      soup = bs(r.text,"html.parser")
+      list = soup.select(".list table tr")
+      data = '{"data": ['
+      list.pop(0)
+      for item in list:
+        link = item.find("a")["href"]
+        img = item.find("img")["data-srcset"].split(",")
+        type = item.findAll("td")[2].text.replace("\n","").strip(" ")
+        img1 = img[0].split()[0]
+        img2 = img[1].split()[0]
+        title = (item.findAll("a")[1].text)
+        while True:
+          if title.find('\n') == -1:
+            break
+          title = title.replace('"',"")
+        data = data +  "{"+f'"type": "{type}","title": "{title}", "link": "{link}", "img" : ["{img1}","{img2}"]'+"},"
+      jsonData = data[0:-1] + ']}'
+      return json.loads(jsonData)
 
 
-(myanimelist.search("naruto"))
+
+if __name__ == "__main__":
+  print(myanimelist.search("naruto"))
