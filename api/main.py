741a491b8aa69f96af68dacaa2a600dfbfb912d0
codewithap <99323868+codewithap@users.noreply.github.com>
2023-02-25 16:01:43 +0530

    Add files via upload

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from scrappers.manganelo import manganelo
 from scrappers.myanimelist import myanimelist
+from scrappers.mangafire import mangafire
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -8,23 +9,36 @@ cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.route("/")
 def home():
-  
-  return ""
+  return "🥳🥳🥳🎉🎉🎉🎉🎉"
 
-@app.route("/api/v1/search")
-def search():
-  query = request.args.get("q")
-  data = myanimelist.search(query)
+## myanimelist search ##
+@app.route("/api/v1/search/<string:q>")
+def search(q):
+  data = myanimelist.search(q)
   return data
-  
-  
-@app.route("/api/v1/manga")
-def getmanga():
-  chapters = request.args.get("id")
-  return ""
 
+## mangafire search ##
+@app.route("/api/v1/search/mangafire/<q>")
+def mangaFireSearch(q):
+  data = mangafire.search(q)
+  return data
+
+## myanimelist manga info ##
+@app.route("/api/v1/manga/<id>")
+def anime(id):
+  data = myanimelist.manga(id)
+  return data
+
+## get chapters from mangafire ##
+@app.route("/api/v1/chapters/mangafire/<name>")
+def chapters(name): 
+  mangaFireId = mangafire.search(name)
+  chapData = mangafire.search(mangafireId)
+  return chapData
+
+## list of top animes from manga fire ##
 @app.route("/api/v1/top")
-def topManga():
+def topAnime(): 
   page = request.args.get("page")
   data = myanimelist.topMangas(page)
   return data
